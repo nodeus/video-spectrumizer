3dc063510c6da29a582ece21b291db21fd0a7a01
nodeus <nodeus@yandex.ru>
2025-06-04 03:04:00 +0500

    Добавлена поддержка .ini файлов конфигурации

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/nodeus/video-spectrumizer
+
+go 1.24.2
+
+require gopkg.in/ini.v1 v1.67.0 // indirect
--- main.go
+++ main.go
@@ -11,29 +11,51 @@ import (
 	"runtime"
 	"strings"
 	"sync"
+
+	"gopkg.in/ini.v1"
 )
 
 type Config struct {
-	InputVideo    string
-	OutputVideo   string
-	TempDir       string
-	Framerate     float64
-	ScaleFactor   int
-	AudioBitrate  string
-	EncoderType   string
-	ConfigFile    string
-	ImgConverter  string
-	DeleteTemp    bool
-	PauseBefore   bool
-	Threads       int
-	ResizeWidth   int
-	ResizeHeight  int
-	ShowFFmpegOut bool
+	InputVideo    string  `ini:"input"`
+	OutputVideo   string  `ini:"output"`
+	TempDir       string  `ini:"temp"`
+	Framerate     float64 `ini:"fps"`
+	ScaleFactor   int     `ini:"scale"`
+	AudioBitrate  string  `ini:"audio-bitrate"`
+	EncoderType   string  `ini:"encoder"`
+	ConfigFile    string  `ini:"config"`
+	ImgConverter  string  `ini:"converter"`
+	DeleteTemp    bool    `ini:"cleanup"`
+	PauseBefore   bool    `ini:"pause"`
+	Threads       int     `ini:"threads"`
+	ResizeWidth   int     `ini:"width"`
+	ResizeHeight  int     `ini:"height"`
+	ShowFFmpegOut bool    `ini:"verbose-ffmpeg"`
 }
 
+const defaultConfigFile = "config.ini"
+
 func main() {
-	config := parseFlags()
-	validateConfig(config)
+	// Предварительная обработка флага -config
+	configFile := defaultConfigFile
+	if len(os.Args) > 1 {
+		for i, arg := range os.Args[1:] {
+			if arg == "-config" && i+1 < len(os.Args)-1 {
+				configFile = os.Args[i+2]
+				break
+			} else if strings.HasPrefix(arg, "-config=") {
+				configFile = strings.SplitN(arg, "=", 2)[1]
+				break
+			}
+		}
+	}
+
+	// Загрузка конфигурации
+	config := loadDefaultConfig()
+	if err := loadIniConfig(configFile, config); err != nil {
+		log.Printf("Ошибка загрузки конфига: %v. Используются значения по умолчанию", err)
+	}
+	parseFlags(config)
 
 	// Получаем абсолютные пути для всех компонентов
 	config = resolvePaths(config)
@@ -77,27 +99,63 @@ func main() {
 	log.Println("Обработка завершена!")
 }
 
-func parseFlags() *Config {
-	config := &Config{}
+func loadDefaultConfig() *Config {
+	return &Config{
+		InputVideo:    "",
+		OutputVideo:   "video-out.mp4",
+		TempDir:       "temp",
+		Framerate:     25.0,
+		ScaleFactor:   8,
+		AudioBitrate:  "384k",
+		EncoderType:   "nvidia",
+		ConfigFile:    "conv.isw",
+		ImgConverter:  "img2spectrum.exe",
+		DeleteTemp:    true,
+		PauseBefore:   true,
+		Threads:       runtime.NumCPU(),
+		ResizeWidth:   -1,
+		ResizeHeight:  192,
+		ShowFFmpegOut: true,
+	}
+}
 
-	flag.StringVar(&config.InputVideo, "input", "", "Входной видеофайл (обязательно)")
-	flag.StringVar(&config.OutputVideo, "output", "video-out.mp4", "Выходной видеофайл")
-	flag.StringVar(&config.TempDir, "temp", "temp", "Директория для временных файлов")
-	flag.Float64Var(&config.Framerate, "fps", 25.0, "Частота кадров")
-	flag.IntVar(&config.ScaleFactor, "scale", 8, "Масштаб увеличения")
-	flag.StringVar(&config.AudioBitrate, "audio-bitrate", "384k", "Битрейт аудио")
-	flag.StringVar(&config.EncoderType, "encoder", "nvidia", "Тип кодировщика (cpu/nvidia/amd)")
-	flag.StringVar(&config.ConfigFile, "config", "conv.isw", "Конфиг для img2spectrum")
-	flag.StringVar(&config.ImgConverter, "converter", "f:/Portable/img2spec/img2spectrum.exe", "Путь к конвертеру")
-	flag.BoolVar(&config.DeleteTemp, "cleanup", true, "Удалять временные файлы")
-	flag.BoolVar(&config.PauseBefore, "pause", true, "Пауза перед конвертацией")
-	flag.IntVar(&config.Threads, "threads", runtime.NumCPU(), "Количество потоков")
-	flag.IntVar(&config.ResizeWidth, "width", 256, "Ширина после ресайза")
-	flag.IntVar(&config.ResizeHeight, "height", -1, "Высота после ресайза")
-	flag.BoolVar(&config.ShowFFmpegOut, "verbose-ffmpeg", true, "Показывать вывод FFmpeg")
+func loadIniConfig(filename string, config *Config) error {
+	iniFile, err := ini.Load(filename)
+	if err != nil {
+		return err
+	}
+
+	// Загружаем только секцию [default]
+	section := iniFile.Section("default")
+	if err = section.MapTo(config); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func parseFlags(config *Config) {
+	flag.StringVar(&config.InputVideo, "input", config.InputVideo, "Входной видеофайл (обязательно)")
+	flag.StringVar(&config.OutputVideo, "output", config.OutputVideo, "Выходной видеофайл")
+	flag.StringVar(&config.TempDir, "temp", config.TempDir, "Директория для временных файлов")
+	flag.Float64Var(&config.Framerate, "fps", config.Framerate, "Частота кадров")
+	flag.IntVar(&config.ScaleFactor, "scale", config.ScaleFactor, "Масштаб увеличения")
+	flag.StringVar(&config.AudioBitrate, "audio-bitrate", config.AudioBitrate, "Битрейт аудио")
+	flag.StringVar(&config.EncoderType, "encoder", config.EncoderType, "Тип кодировщика (cpu/nvidia/amd)")
+	flag.StringVar(&config.ConfigFile, "config", config.ConfigFile, "Конфиг для img2spectrum")
+	flag.StringVar(&config.ImgConverter, "converter", config.ImgConverter, "Путь к конвертеру")
+	flag.BoolVar(&config.DeleteTemp, "cleanup", config.DeleteTemp, "Удалять временные файлы")
+	flag.BoolVar(&config.PauseBefore, "pause", config.PauseBefore, "Пауза перед конвертацией")
+	flag.IntVar(&config.Threads, "threads", config.Threads, "Количество потоков")
+	flag.IntVar(&config.ResizeWidth, "width", config.ResizeWidth, "Ширина после ресайза")
+	flag.IntVar(&config.ResizeHeight, "height", config.ResizeHeight, "Высота после ресайза")
+	flag.BoolVar(&config.ShowFFmpegOut, "verbose-ffmpeg", config.ShowFFmpegOut, "Показывать вывод FFmpeg")
+
+	// Специальный флаг для указания конфиг-файла (не сохраняется в структуре)
+	var configFile string
+	flag.StringVar(&configFile, "config-file", defaultConfigFile, "Путь к INI-конфигу")
 
 	flag.Parse()
-	return config
 }
 
 // Преобразование путей в абсолютные
