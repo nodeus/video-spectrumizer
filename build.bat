4ac0fbe7cdb9600590c32ffe6436828199f12bb8
nodeus <nodeus@yandex.ru>
2025-06-07 01:45:51 +0500

    Автоматическое добавление версии к имени файла, копирование бинарника в папку .\bin

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
 set version="v1.0.3"
 for /f "delims=" %%a in ('git log -1 --pretty^=format:"%%an (%%ae)"') do set "user=%%a"
-go build -o video-spectrumizer.exe -v -ldflags="-X 'main.Version=%version%' -X 'main.BuildUser=%user%' -X 'main.BuildTime=%date% | %time%'" main.go
+go build -o video-spectrumizer_%version%.exe -v -ldflags="-X 'main.Version=%version%' -X 'main.BuildUser=%user%' -X 'main.BuildTime=%date% | %time%'" main.go
+copy video-spectrumizer_%version%.exe .\bin\video-spectrumizer_%version%.exe
